d44a7366e9b076c27e09c7dc40a98c870317a309
Bharat Soni <bharatchills@gmail.com>
2024-01-06 01:12:56 +0530

    add: showing only last 5 results

--- src/App.tsx
+++ src/App.tsx
@@ -44,7 +44,11 @@ export default function App() {
   }
   //function to get accounts from local storage
   function getAccountLocalStorage() {
-    return getLocalStorage("accounts");
+    const accounts = getLocalStorage("accounts");
+    if (accounts) {
+      return accounts.slice(-5);
+    }
+    return [];
   }
 
   const onSubmit = (e: any) => {
@@ -125,7 +129,7 @@ export default function App() {
             label="All of these words"
             value={word()}
             onChange={(e) => setWord(e.target.value)}
-            subtext={`Example: agi`}
+            subtext={`Example: twitter`}
           />
 
           <TextInputComponent
@@ -207,21 +211,34 @@ function TextInputComponent(props: TextInputComponentProps) {
     console.log(value);
     props.onChange({ target: { value: value } });
   }
+  const [list, setList] = createSignal(props.recommended??[]);
+
+  //filter recommended list based on input value
+  function filterRecommendedList(value: string) {
+    if (props.recommended) {
+      setList(
+        props.recommended.filter((item) => {
+          return item.toLowerCase().includes(value.toLowerCase());
+        })
+      );
+    }
+  }
   return (
     <div class="relative group w-full min-w-[200px] my-4">
       <input
         class="peer w-full h-full bg-white dark:bg-black text-black dark:text-white font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-gray-700 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-gray-800 placeholder-shown:border-t-gray-800 border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-gray-300 dark:border-gray-800 focus:border-blue-500"
         placeholder=""
         onChange={props.onChange}
+        oninput={(e) => filterRecommendedList(e.target.value)}
         value={props.value ? props.value : ""}
       />
       <label class="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate peer-placeholder-shown:text-gray-500 leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-gray-500 transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-gray-500 peer-focus:text-blue-500 before:border-gray-800 peer-focus:before:!border-blue-500 after:border-gray-800 peer-focus:after:!border-blue-500">
         {props.label}
       </label>
       {/* recommended list show when input tag is on focus */}
-      {props.recommended && (
+      {list() && (
         <div class=" hidden group-focus-within:block absolute z-10 w-full bg-white dark:bg-black border border-gray-300 dark:border-gray-800 rounded-b-lg shadow-lg">
-          {props.recommended.map((item) => (
+          {list().map((item) => (
             <div onmouseover={()=>setRecommendedValue(item)} class="px-3 py-2.5 text-black dark:text-white hover:bg-gray-100 dark:hover:bg-gray-900 cursor-pointer">
               {item}
             </div>
